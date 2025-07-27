3f07c8bc439aee6a3bdd28c6c15b5c2d59a2f828
dariodf <dario@getsavi.com>
2025-07-27 14:19:47 -0300

    More flexible configs

--- lib/snatch_ex.ex
+++ lib/snatch_ex.ex
@@ -4,14 +4,17 @@ defmodule SnatchEx do
   alias SnatchEx.{Fetcher, Scraper, Renderer}
 
   def snatch(search_text) do
-    Enum.find_value(sites(), fn site ->
-      with {:ok, html} <- Fetcher.search(site["search_url"], search_text),
-           base_url <- URI.to_string(%{URI.parse(site["search_url"]) | query: nil}),
-           {:ok, [first_result | _]} <-
-             Scraper.extract_results(html, site["results_selector"], base_url),
-           {:ok, target_html} <- Fetcher.fetch_page(first_result),
-           {:ok, data} <- Scraper.extract_contents(target_html, site["content_selectors"]),
-           {:ok, pdf_binary} <- Renderer.to_pdf(site["template"], data) do
+    config = config()
+
+    Enum.find_value(config["search"], fn {search_url, results_selectors} ->
+      with [%{"selector" => selector, "attribute" => attribute} | _] = results_selectors,
+           {:ok, html} <- Fetcher.search(search_url, search_text),
+           base_url <- URI.to_string(%{URI.parse(search_url) | query: nil}),
+           {:ok, [target_url | _]} <-
+             Scraper.extract_results(html, selector, attribute, base_url),
+           {:ok, target_html} <- Fetcher.fetch_page(target_url),
+           {:ok, data} <- Scraper.extract_contents(target_url, target_html, config["scraping"]),
+           {:ok, pdf_binary} <- Renderer.to_pdf(config["rendering"]["template"], data) do
         {:ok, pdf_binary}
       else
         {:ok, []} -> :no_results
@@ -19,14 +22,20 @@ defmodule SnatchEx do
     end)
   end
 
-  defp sites do
+  defp config do
     yaml = """
-    - search_url: "https://gametabs.net/tabs?search={query}"
-      results_selector: "div.results-list > a"
-      content_selectors:
+    search:
+      "https://gametabs.net/tabs?search={query}":
+        - selector: "div.results-list > a"
+          attribute: "href"
+
+    scraping:
+      "https://gametabs.net":
         content: "pre"
         name: "#tab-page-tab-name"
         band: "#tab-page-game-name"
+
+    rendering:
       template: |
         {name} - {band}
 
--- lib/snatch_ex/scraper.ex
+++ lib/snatch_ex/scraper.ex
@@ -1,14 +1,21 @@
 defmodule SnatchEx.Scraper do
-  def extract_results(html, selector, base_url) do
+  def extract_results(html, selector, attribute, base_url) do
     html
     |> Floki.parse_document!()
     |> Floki.find(selector)
-    |> Floki.attribute("href")
+    |> Floki.attribute(attribute)
     |> Enum.map(&(base_url |> URI.merge(&1) |> URI.to_string()))
     |> then(&{:ok, &1})
   end
 
-  def extract_contents(html, selectors) do
+  def extract_contents(url, html, scraping_configs) do
+    url_host = URI.parse(url).host
+
+    selectors =
+      Enum.find_value(scraping_configs, [], fn {config_url, selectors} ->
+        if url_host == URI.parse(config_url).host, do: selectors
+      end)
+
     Enum.map(selectors, fn {key, selector} ->
       value =
         html
